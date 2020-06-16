aa8185af44d080fdd8103d43f417b98fb95da8de
Boris <yubo10019@adpanshi.com>
2020-06-16 19:04:39 +0800

    gather all the time

--- src/main/java/com/example/demo/encrypt/TestSecurityUtil.java
+++ src/main/java/com/example/demo/encrypt/TestSecurityUtil.java
@@ -9,7 +9,10 @@ import com.example.demo.user.SecurityUtil;
 public class TestSecurityUtil {
 
     public static void main(String[] args) {
-        String ciphertext = jiami();
+        String id = "2849441";
+        String salt = "bd4273038b5a85bc8ca880aa1773d77c7ffffe8d00cce396167ae4b135b5164b";
+        String password = "123456";
+        jiami(id, salt, password);
     }
 
     /**
@@ -25,6 +28,20 @@ public class TestSecurityUtil {
         System.out.println("salt:" + salt);
         Integer id = 33333;
         String password = "123456";
+        String ciphertext = SecurityUtil.getStoreLogpwd(String.valueOf(id), password , salt);
+        System.out.println(ciphertext);
+        return ciphertext;
+    }
+
+    /**
+     * 非对称加密——用于处理用户的密码
+     * 1、将用户id、用户输入的密码，程序生成的随机数salt 通过算法 生成的密文字符串
+     * 2、将用户id，程序生成的随机数salt，密文字符串
+     * 3、校验，根据用户id、用户输入的密码，加上salt，用步骤一的算法生成一个密码字符串，然后和数据库的密码字符串比对，
+     *    相同校验通过，不相同校验不通过
+     * @return
+     */
+    private static String  jiami(String id, String salt, String password) {
         String ciphertext = SecurityUtil.getStoreLogpwd(String.valueOf(id), password , salt);
         System.out.println(ciphertext);
         return ciphertext;
